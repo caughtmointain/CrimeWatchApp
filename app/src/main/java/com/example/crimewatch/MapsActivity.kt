f14fb4b8515b26d45489d7698ee1d475c7364521
caughtmountain <najreh50@gmail.com>
2020-04-14 00:34:27 -0500

    adding code to point to Frisco

--- app/src/main/java/com/example/crimewatch/MapsActivity.kt
+++ app/src/main/java/com/example/crimewatch/MapsActivity.kt
@@ -36,8 +36,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        val sydney = LatLng(33.1,-96.8)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Frisco"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
